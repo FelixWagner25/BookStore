6b18b344724cbd3be1e4c4b277844b22de8cef3d
Felix Wagner <post@felixwagner.eu>
2025-01-26 19:04:15 +0100

    add book-card img to book card

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,5 +1,14 @@
 function getBookTemplate(indexObject) {
   return `
-  <div class='book-card'> <b>${books[indexObject].name} </b></div>
+  <div class='book-card'>
+  <div class='book-card-title'> 
+  <b>${books[indexObject].name}</b>
+  </div>
+  <hr class='hr-book-card'>
+  <br>
+  <img src='../assets/img/iconmonstr-book-2-240.png' class='center book-card-img'>
+  <br>
+  <hr class='hr-book-card'>
+  </div>
   `;
 }
