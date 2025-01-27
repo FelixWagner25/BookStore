beb800a5f539a115800ddfa6bd0f8626af18d8ab
Felix Wagner <post@felixwagner.eu>
2025-01-27 11:12:20 +0100

    extend book card

--- scripts/templates.js
+++ scripts/templates.js
@@ -10,26 +10,31 @@ function getBookTemplate(indexObject) {
   <br>
   <hr class='hr-book-card'>
   <br>
-  <div class='price-like'>
+  <div class='price-like book-card-pd-lr'>
   <b class='color-red'>${books[indexObject].price} EUR</b>
   <div class='heart-counter'>
   ${books[indexObject].likes}
   <img class='like-img' src="../assets/img/RedHeartEmpty_MaterialIcons.png">
   </div>
   </div>
-  <div class='book-info'>
-  <b>${books[indexObject].author}</b>
+  <div class='book-info book-card-pd-lr'>
+  <b>Author</b>:
+  <span>${books[indexObject].author}</span>
+  <b>Erscheinungsjahr</b>:
+  <span>${books[indexObject].publishedYear}</span>
+  <b>Genre</b>:
+  <span>${books[indexObject].genre}</span>
   </div>
   <br>
   <hr class='hr-book-card'>
   <br>
-  <div class='book-info'>
+  <b class='book-card-pd-lr'>Kommentare:</b>
+  <br>
+  <div class='book-comments book-card-pd-lr'>
   </div>
   <br>
   <hr class='hr-book-card'>
   <br>
-  <div class='book-comments'>
-  </div>
   </div>
   `;
 }
