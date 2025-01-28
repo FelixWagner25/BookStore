137f13cb4d6a9a1371f65237876e63c941314009
Felix Wagner <post@felixwagner.eu>
2025-01-28 10:52:43 +0100

    add fct getCommentLineTemplate()

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ function loadBooks() {
 
   for (let indexBook = 0; indexBook < books.length; indexBook++) {
     contentRef.innerHTML += getBookTemplate(indexBook);
+
     let bookCommentRefId = String("book-comments" + indexBook);
     let bookCommentRef = document.getElementById(bookCommentRefId);
     bookCommentRef.innerHTML = "";
-    let allBookComments = [];
+
     for (
       let indexComment = 0;
       indexComment < books[indexBook].comments.length;
       indexComment++
     ) {
-      let commentName = books[indexBook].comments[indexComment].name;
-      let commentText = books[indexBook].comments[indexComment].comment;
-      allBookComments.push({ name: commentName, comment: commentText });
+      bookCommentRef.innerHTML += getBookCommentLineTemplate(
+        indexBook,
+        indexComment
+      );
     }
-    console.table(allBookComments);
   }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -39,16 +39,24 @@ function getBookTemplate(indexObject) {
   `;
 }
 
-function getBookCommentsTemplate(indexObject) {
-  let allBookComments = [];
-  for (
-    let indexComment = 0;
-    indexComment < books[indexObject].comments.length;
-    indexComment++
-  ) {
-    let commentName = books[indexObject].comments[indexComment].name;
-    let commentText = books[indexObject].comments[indexComment].comment;
-    allBookComments.push({ name: commentName, comment: commentText });
-  }
-  return console.table(allBookComments);
+// function getBookCommentsTemplate(indexObject) {
+//   let allBookComments = [];
+//   for (
+//     let indexComment = 0;
+//     indexComment < books[indexObject].comments.length;
+//     indexComment++
+//   ) {
+//     let commentName = books[indexObject].comments[indexComment].name;
+//     let commentText = books[indexObject].comments[indexComment].comment;
+//     allBookComments.push({ name: commentName, comment: commentText });
+//   }
+//   return console.table(allBookComments);
+//}
+
+function getBookCommentLineTemplate(indexBook, indexComment) {
+  let commentName = books[indexBook].comments[indexComment].name;
+  let commentText = books[indexBook].comments[indexComment].comment;
+  return `
+  <span class='break-word'>[${commentName}]</span> : <span class='break-word'>${commentText}</span>
+  `;
 }
